705e49139b1628d71d26272d925ddd6534114cec
diegogodinezr <godinezroblesdiego@gmail.com>
2024-05-28 13:49:13 -0600

    error solucionado

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import React, { useState, useEffect } from 'react';
 import Hangman from './components/Hangman';
 import Welcome from './components/Welcome';
 
+// Definición de la interfaz para wordCategories
+interface WordCategories {
+  tecnologia: string[];
+  profesiones: string[];
+  paises: string[];
+  alimentos: string[];
+}
 
 const App: React.FC = () => {
   const [gameStarted, setGameStarted] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState('');
   const [selectedCategoryWords, setSelectedCategoryWords] = useState<string[]>([]);
 
+
+  
   
 
-  const wordCategories = {
+  const wordCategories: WordCategories = {
     tecnologia: ['javascript', 'react', 'nodejs', 'express', 'mongodb', 'python', 'django', 'flask', 'java', 'spring', 'hibernate', 'mysql', 'postgresql', 'sql', 'html', 'css', 'sass', 'less'],
     profesiones: ['doctor', 'ingeniero', 'arquitecto', 'programador', 'diseñador', 'maestro', 'enfermero', 'abogado', 'cientifico'],
     paises: ['estadosunidos', 'mexico', 'canada', 'brasil', 'argentina', 'españa', 'francia', 'italia', 'alemania', 'japon'],
